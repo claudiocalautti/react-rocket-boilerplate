54b686d0b0c2ee62c1fb00febae9c4453a1bfd6a
claudiocalautti <claudio@creativefrontend.com>
2015-10-11 11:20:35 +0100

    Fixed position errorHandler in sass task.

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -16,8 +16,8 @@ gulp.task('sass', function () {
       sourceMap: 'sass',
       outputStyle: global.isProd ? 'compressed' : 'nested'
     }))
-    .pipe(autoprefixer("last 2 versions", "> 1%", "ie 8"))
     .on('error', handleErrors)
+    .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
     .pipe(gulp.dest(config.styles.dest))
     .pipe(gulpif(browserSync.active, browserSync.reload({ stream: true })));
 
